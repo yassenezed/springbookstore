c98fb78305eced6f81c5c63468b25ea25d7b441f
Yassine <yassinezaid668@gmail.com>
2024-04-20 16:48:21 +0000

    First Version

---
+++ src/main/java/com/example/bookstorespringboot/BookStoreSpringBootApplication.java
@@ -0,0 +1,13 @@
+package com.example.bookstorespringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookStoreSpringBootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BookStoreSpringBootApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/bookstorespringboot/BookStoreSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.bookstorespringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookStoreSpringBootApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
